020fe685bd7076adbc29943f1daa0ec685bd15b0
Flávio Gimenes <162215669+flavingimenes@users.noreply.github.com>
2025-09-15 23:21:33 -0400

    Update ErrorPage.jsx

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div className='error-page'>
         <p>Erro 404!</p>
-        <p className='error-msg'>Página não encontrada</p>
+        <p className='error-msg'>Página não encontrada...</p>
     </div>
   )
 }
