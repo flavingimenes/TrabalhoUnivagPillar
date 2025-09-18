e0763547f3fb97e3c6806c9a8dec716da6386831
Flávio Gimenes <162215669+flavingimenes@users.noreply.github.com>
2025-09-17 23:00:11 -0400

    work progression

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,6 +5,8 @@ import Home from '../pages/Home/Home'
 import { Link } from 'react-router-dom'
 import Materias from '../pages/materias/Materias'
 import login from '../pages/Forms/Login'
+import { FaHome, FaBook } from "react-icons/fa";
+
 
 const SideBar = () => {
   return (
@@ -14,8 +16,8 @@ const SideBar = () => {
         <h2 style={{fontSize: "20px", marginTop: "20px", marginBottom: "20px"}}>Bom dia, aluno</h2>
         <hr className='line'/>
         <div style={{display:"grid"}}>
-            <Link to="/home" className='sidebar-link'>Página Inicial </Link>
-            <Link to="/materias" className='sidebar-link'>Matérias</Link>
+            <Link to="/home" className='sidebar-link'><FaHome className='icon-sidebar'/>Página Inicial </Link>
+            <Link to="/materias" className='sidebar-link'><FaBook className='icon-sidebar'/>Matérias</Link>
         </div>
         <Link to="/" className='logout-bottom'>Sair</Link>
     </div>
