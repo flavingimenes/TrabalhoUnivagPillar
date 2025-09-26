d1025dff616e52a4b2a13df843cf2331fa70ad4b
Flávio Gimenes <162215669+flavingimenes@users.noreply.github.com>
2025-09-25 21:56:31 -0400

    the materials beta adictions
    trust me, it's really on the beta

--- src/pages/materias/PrimeiroAno/PrimeiroAno.jsx
+++ src/pages/materias/PrimeiroAno/PrimeiroAno.jsx
@@ -9,13 +9,44 @@ const PrimeiroAno = () => {
     <>
       <div className='root-home'>
         <SideBar />
-        <div className='mainContent'>
+        <div className='main-div'>
 
           <header className='header-container'>
-            <img src={HeaderImg} alt="HeaderImg" className='header-img'/>
-              <FaBook className='book-header' />
+            <img src={HeaderImg} alt="HeaderImg" className='header-img' />
+            <FaBook className='book-header' />
           </header>
+          <div className='header-text-main'>
+            <h1>
+              1º Ano do Ensino Médio
+            </h1>
+          </div>
+          <div className='main-content'>
+            <h3 className='material-name'>
+              Linguagens e suas Tecnologias
+            </h3>
+            <h4>
+              ● Língua Portuguesa:
+            </h4>
+            <p>
+              <br />○ Habilidades: Leitura e interpretação de textos literários e não literários. Análise da
+              estrutura de gêneros textuais (notícia, crônica, conto). Uso da norma-padrão e
+              compreensão de variações linguísticas. <br /><br />
+              ○ Competências: Compreender a linguagem como forma de interação social, de
+              expressão e de acesso à informação. Desenvolver a argumentação e o pensamento
+              crítico na leitura e na produção textual.
 
+            </p>
+            <br /><br />
+            <p>
+              <br />○ Habilidades: Leitura e interpretação de textos literários e não literários. Análise da
+              estrutura de gêneros textuais (notícia, crônica, conto). Uso da norma-padrão e
+              compreensão de variações linguísticas. <br /><br />
+              ○ Competências: Compreender a linguagem como forma de interação social, de
+              expressão e de acesso à informação. Desenvolver a argumentação e o pensamento
+              crítico na leitura e na produção textual.
+
+            </p>
+          </div>
 
         </div>
       </div>
